da29f0bb17c138eab98b0e4ef1ea78c8ea07f616
randy <randyh1981@gmail.com>
2020-02-11 17:53:52 -0500

    first chance of Deck

--- cards.py
+++ cards.py
@@ -1,5 +1,24 @@
 #! /usr/bin/python3.8
+import random
 
-class card():
+class Card():
     def __init__(self, cost):
         self.cost = cost
+
+class Deck():
+    def __init__(self):
+        self.deck = []
+    
+    def add(self, card):
+        if isinstance(card, Card):
+            self.deck.append(card)
+
+    def DrawCard(self):
+        return self.deck.pop(0)
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def remove(self,card):
+        self.deck.remove(card)
+
