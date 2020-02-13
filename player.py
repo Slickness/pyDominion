0ad6ef8d16fd7473d23e992fed20a6fd438a27c4
randy <randyh1981@gmail.com>
2020-02-12 19:06:02 -0500

    added victory cards

--- player.py
+++ player.py
@@ -1,17 +1,28 @@
 #! /usr/bin/python3.8
 from cards import Deck, Card
+import cards
+import copy
 
 class Player():
     def __init__(self, name):
         self.name = name
         self.hand = Deck()
         self.pile = Deck()
+        self.money = 0
         self.discard = Deck()
+        self.startDeck()
+        self.drawHand()
+        self.actions = 1
+        self.buys  = 1
 
     def endTurn(self):
         for card in reversed(self.hand.deck):
             self.discard.add(card)
             self.hand.remove(card)
+        self.drawHand()
+        self.actions = 1
+        self.buys  = 1
+        self.money = 0
 
     def emptyDiscard(self):
         for card in reversed(self.discard.deck):
@@ -24,13 +35,35 @@ class Player():
             self.emptyDiscard()
         self.hand.add(self.pile.drawCard())
 
+    def addMoney(self, value):
+        self.money = self.money + value
+
     def drawHand(self):
         '''since you start with 10 cards you should never have less than 5 cards'''
-        for x in range(5):
+        for _x in range(5):
             if len(self.pile.deck) == 0:
                 self.emptyDiscard()
-            self.drawCardPile(self)
+            self.drawCardPile()
+        for card in self.hand.deck:
+            if card.kind == "T":
+                self.addMoney(card.value)
+                
+    
+    def startDeck(self):
+
+        '''Each player will start with 7 copper and 3 estates '''
+        for _x in range(7):
+            self.pile.add(copy.deepcopy(cards.copper))
+        for _x in range(3):
+            self.pile.add(copy.deepcopy(cards.estate))
+        self.pile.shuffle()
+    
+  
+# testing out the player cll
+
 
 test = Player("randy")
 
-print (test.drawHand())
+for card in test.hand.deck:
+    print (card.name)
+print (test.money)
